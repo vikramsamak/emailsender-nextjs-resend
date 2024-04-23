a78eaae7289acc3b788ad927e8c96af4acd8e3d9
vikramsamak <vikramsamak02@gmail.com>
2024-04-23 11:29:06 +0530

    Added code to display current hour.

--- src/EmailTemplates/testTemplate.js
+++ src/EmailTemplates/testTemplate.js
@@ -1,11 +1,14 @@
 import TailwindWrapper from "@/components/TailwindWrapper";
-import { Container, Button, Text } from "@react-email/components";
+import { Container, Text } from "@react-email/components";
 
 export default function Testemailtemplate() {
+  const currentHour = new Date().getHours();
+
   return (
     <TailwindWrapper>
-      <Container className="flex justify-center items-center">
+      <Container className="flex flex-col justify-center items-center">
         <Text>This is system generated mail sent by cronjob-next app.</Text>
+        <Text>Current Hour: {currentHour}</Text>
       </Container>
     </TailwindWrapper>
   );
