0624f7fa605a954db7bdab9c8dd0f92205b02707
vikramsamak <vikramsamak02@gmail.com>
2024-05-04 17:16:45 +0530

    Some changes.

--- src/EmailTemplates/testTemplate.js
+++ src/EmailTemplates/testTemplate.js
@@ -10,6 +10,7 @@ export default function Testemailtemplate() {
       <Container className="flex flex-col justify-center items-center">
         <Text>This is system generated mail sent by cronjob-next app.</Text>
         <Text>Current Hour: {currentTime}</Text>
+        <Text>Current Env {process.env.NODE_ENV}</Text>
       </Container>
     </TailwindWrapper>
   );
