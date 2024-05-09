ae597f3e9c82224bdd334b7c2ec12c81bd446a94
vikramsamak <vikramsamak02@gmail.com>
2024-05-09 08:31:21 +0530

    Email sending lib change

--- src/app/api/emailsender/route.js
+++ src/app/api/emailsender/route.js
@@ -1,34 +1,25 @@
 import { getIndianTime } from "@/helpers/getIndianTime";
+import { resend } from "@/libs/resend";
 import emailjs from "@emailjs/nodejs";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const templateParams = {
-      emailtext: `This is system generated mail sent by cronjob-next app. Current Time: ${getIndianTime(
+    const { data, error } = await resend.emails.send({
+      from: "noreply@vikramsamak.com",
+      to: ["vikramsamak02@gmail.com"],
+      subject: "Hello world",
+      text: `This is system generated mail sent by cronjob-next app. Current Time: ${getIndianTime(
         new Date()
       )} Current Env ${process.env.NODE_ENV}`,
-    };
+    });
 
-    try {
-      const emailRes = await emailjs.send(
-        process.env.EMAILJS_SERVICE_ID,
-        process.env.EMAILJS_TEMPLATE_ID,
-        templateParams,
-        {
-          publicKey: process.env.EMAILJS_PUBLIC_KEY,
-          privateKey: process.env.EMAILJS_PRIVATE_KEY,
-        }
-      );
+    if (data) {
+      return NextResponse.json({ msg: "Email sent sucessfully." });
+    }
 
-      if (emailRes.status === 200) {
-        return NextResponse.json({ msg: "Email sucessfully sent" });
-      } else {
-        return NextResponse.json({ error: "Failed to send email" });
-      }
-    } catch (err) {
-      console.log("EMAILJS FAILED...", err);
-      return NextResponse.json({ error: "Failed to send email" });
+    if (error) {
+      return NextResponse.json({ error: "Failed to send email." });
     }
   } catch (error) {
     console.log(error);
