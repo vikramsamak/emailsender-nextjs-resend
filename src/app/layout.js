bf2e89be658ffaac53f8a46baa6701885f7b7146
vikramsamak <vikramsamak02@gmail.com>
2024-05-11 22:41:47 +0530

    Shadcn initial setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,29 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import { Inter as FontSans } from "next/font/google";
 
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 export const metadata = {
-  title: "Cronjob-Next App",
-  description: "Cronjob demo app build with nextjs",
+  title: "EmailSender-NextApp",
+  description: "Simple app build with nextjs which can send emails.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { getBaseUrl } from "@/helpers/getBaseUrl";
 import { useState } from "react";
 
@@ -35,13 +36,13 @@ export default function Home() {
     <main className="flex h-screen items-center justify-center p-24">
       <div className="flex flex-col p-4 border shadow-lg rounded-lg gap-4">
         <h1 className="text-2xl">CRONJOB - NEXT</h1>
-        <button
-          className="p-4 rounded-md bg-blue-500 hover:text-white"
+        <Button
+          className="p-4 rounded-md"
           onClick={sendTestEmail}
           disabled={loading}
         >
           {loading ? "Sending" : "Send test email"}
-        </button>
+        </Button>
       </div>
     </main>
   );
